6e2c471795d0439ff90e26a766f143edde2221e5
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2020-07-13 03:45:07 +0530

    [+] Little upgrade as per requirement.

--- interactor.py
+++ interactor.py
@@ -36,6 +36,7 @@ def scan(url):
     else:
         print(bcolors.BOLD + bcolors.FAIL + '[-] Found negative to saycheese' + bcolors.ENDC)
         scan_status.SAYCHEESE = False
+    
 
     #shellphish
     print(bcolors.BOLD + bcolors.HEADER + 'Scanning for shellphish...' + bcolors.ENDC)
@@ -44,6 +45,7 @@ def scan(url):
     if shellphish_positive:
         print(bcolors.BOLD + bcolors.OKGREEN + '[+] Found positive to shellphish' + bcolors.ENDC)
         scan_status.SHELLPHISH = True
+
     else:
         print(bcolors.BOLD + bcolors.FAIL + '[-] Found negative to shellphish' + bcolors.ENDC)
         scan_status.SHELLPHISH = False
@@ -136,29 +138,32 @@ def actions(url):
     else:
         print(bcolors.BOLD + bcolors.FAIL + '[-] This Seems safe.' + bcolors.ENDC)
 
+def main():
+    banner_pre = bcolors.BOLD + 'Internet Check: ' + bcolors.ENDC
+    try:
+        x = connection_manager.connection_check()
+        print(banner_pre + bcolors.BOLD + bcolors.OKGREEN + 'Available' + bcolors.ENDC.join(''))
+        print()
+    
+        try:
+            url = input(bcolors.BOLD + bcolors.OKBLUE + '[*] Enter URL to scan:' + bcolors.ENDC)
+            print(bcolors.BOLD + bcolors.OKBLUE + '[*] Scanning started...' + bcolors.ENDC)                
+            scan(url)
+            print(bcolors.BOLD + bcolors.OKBLUE + '[*] Done' + bcolors.ENDC)
+            print(bcolors.BOLD + bcolors.OKBLUE + 'Action started...' + bcolors.ENDC)
+            actions(url)
+        except Exception as e:
+            print(bcolors.BOLD + bcolors.FAIL + 'Invalid url!!!' + bcolors.ENDC)
+            print(e)
+            pass
+    except:
+        print(banner_pre + bcolors.BOLD + bcolors.FAIL + 'Not Available.' + bcolors.ENDC)
+        exit(0)
+
 if __name__ == "__main__":
     try:
         if sys.argv[1] == '-v' or '--version':
             print(bcolors.BOLD + bcolors.WARNING + 'Antidote v0.1' + bcolors.ENDC)
             exit(0)
     except Exception as e:
-        banner_pre = bcolors.BOLD + 'Internet Check: ' + bcolors.ENDC
-        try:
-            x = connection_manager.connection_check()
-            print(banner_pre + bcolors.BOLD + bcolors.OKGREEN + 'Available' + bcolors.ENDC.join(''))
-            print()
-        
-            try:
-                url = input(bcolors.BOLD + bcolors.OKBLUE + '[*] Enter URL to scan:' + bcolors.ENDC)
-                print(bcolors.BOLD + bcolors.OKBLUE + '[*] Scanning started...' + bcolors.ENDC)                
-                scan(url)
-                print(bcolors.BOLD + bcolors.OKBLUE + '[*] Done' + bcolors.ENDC)
-                print(bcolors.BOLD + bcolors.OKBLUE + 'Action started...' + bcolors.ENDC)
-                actions(url)
-            except Exception as e:
-                print(bcolors.BOLD + bcolors.FAIL + 'Invalid url!!!' + bcolors.ENDC)
-                print(e)
-                pass
-        except:
-            print(banner_pre + bcolors.BOLD + bcolors.FAIL + 'Not Available.' + bcolors.ENDC)
-            exit(0)
+        main()
