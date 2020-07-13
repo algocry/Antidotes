f080ac7c6c1a57d3e2b5332a5a134467b7d26b25
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2020-07-13 20:42:41 +0530

    [+] Updated __init__.py - #1

---
+++ antidote/core/__init__.py
@@ -0,0 +1,9 @@
+'''
+    antidote - A Script kiddies buster tool
+
+    Being a script kid is not bad. But using someone else's 
+    tools and script to harm others is absolutely bad. 
+    And this thing is actually needed right now.
+'''
+__version__ = '0.1.0'
+ 
