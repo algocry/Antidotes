543f8918a3f4ec796549d2688f02bb4ff6f9ec63
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2020-07-13 23:33:56 +0530

    [*] Updated author's info

--- antidote/core/connection_manager.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def connection_check():
-    response = requests.get('https://example.com')
-    if response.status_code == 200:
-        return True
-    else:
-        return False
