fc3e1662b9f4e49bbbd7d1f0a444f5ea42db137a
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2020-07-14 16:48:40 +0530

    [*] Upgraded conncetion tester module

--- antidotes/core/connection_manager.py
+++ antidotes/core/connection_manager.py
@@ -1,7 +1,7 @@
 import requests
 
-def connection_check():
-    response = requests.get('https://example.com')
+def connection_check(url):
+    response = requests.get(url)
     if response.status_code == 200:
         return True
     else:
